45d5c128dcca017d77b064dc1d0d5b1a841a970f
Anne Faber <anne.faber@txstate.edu>
2016-09-14 14:39:34 -0500

    store cas configuration in environment variables

--- cas.js
+++ cas.js
@@ -1,10 +1,10 @@
 var cas = require('grand_master_cas'); 
 
 cas.configure({
-  casHost: "llavero.its.txstate.edu",   
-  casPath: "/cas",                
+  casHost: process.env.CAS_HOST,
+  casPath: process.env.CAS_PATH,
   ssl: true,                        
-  service: "http://localhost:3000/login", // your site
+  service: process.env.CAS_SERVICE_URL,
   sessionName: "cas_user",          // the cas user_name will be at req.session.cas_user (this is the default)
   renew: false,                     // true or false, false is the default
   redirectUrl: '/'            // the route that cas.blocker will send to if not authed. Defaults to '/'
