d1eb40698a398d92bba198ffa0b47eb59b9c2a48
Anne Faber <anne.faber@txstate.edu>
2016-09-15 10:42:00 -0500

    consolidating components

--- client/components/AdminContainer.jsx
+++ client/components/AdminContainer.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import Admin from './Admin.jsx';
 import $ from 'jquery';
+import SearchBar from './SearchBar.jsx';
+import AddNew from './AddNew.jsx';
+import EditIdeas from './EditIdeas.jsx';
+import SelectCategoryContainer from './SelectCategoryContainer.jsx';
+import { Link, IndexLink } from 'react-router';
+
+const ACTIVE = { background: '#e7e7e7', fontWeight: 'bold'}
 
 class AdminContainer extends React.Component {
 
@@ -10,9 +16,20 @@ class AdminContainer extends React.Component {
 
     render(){
         return(
-            <Admin
-                children = {this.props.children}
-            />
+            <div>
+              <nav className="navbar navbar-default">
+              <ul className="nav navbar-nav">
+                <li><IndexLink to="/admin" activeStyle={ACTIVE}>Ideas</IndexLink></li>
+                <li><a href="#">Comments</a></li>
+                <li><Link to="/admin/users" activeStyle={ACTIVE}>Users</Link></li>
+                <li><Link to="/admin/categories" activeStyle={ACTIVE}>Categories</Link></li>
+                <li><Link to="/admin/stages" activeStyle={ACTIVE}>Stages</Link></li>
+              </ul>
+              </nav>
+              <div className="container">
+                {this.props.children || <EditIdeas/>}
+              </div>
+            </div>
         );
     }
 }
