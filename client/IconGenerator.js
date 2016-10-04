e38dfe5aaa4a5a6a51a66d3c84c1f061d43fe190
Anne Faber <anne.faber@txstate.edu>
2016-10-04 09:47:32 -0500

    Don't use actual user ID's in aria labels.  The id should change from one idea to another

--- client/IconGenerator.js
+++ client/IconGenerator.js
@@ -6,11 +6,11 @@ var numcolors = 12;  //12 icon colors specified in CSS
 
 function getIcons(users, idea_id){
     var generatedIcons = [];
-    var generate = seedgen({icon: icons.length, color: numcolors});
+    var generate = seedgen({icon: icons.length, color: numcolors, aria_id: icons.length * numcolors});
 
      for(var i=0; i<users.length; i++){
         var iconData = generate(users[i] + " " + idea_id);
-        generatedIcons.push({id: users[i], icon: icons[iconData.icon], color: "color" + (iconData.color + 1)});
+        generatedIcons.push({id: users[i], icon: icons[iconData.icon], color: "color" + (iconData.color + 1), aria_id: iconData.aria_id});
      }
     return generatedIcons;
 }
