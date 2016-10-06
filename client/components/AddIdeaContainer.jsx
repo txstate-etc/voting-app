f3505bb8d76b6205154abd62c3b9a80fbf616c32
Anne Faber <anne.faber@txstate.edu>
2016-10-06 16:57:42 -0500

    Notify the user that they might be contacted by someone from suppport about their idea submission.

--- client/components/AddIdeaContainer.jsx
+++ client/components/AddIdeaContainer.jsx
@@ -13,34 +13,37 @@ class AddIdeaContainer extends React.Component {
     }
 
     submit(formData){
-        var fd = new FormData();
-        fd.append('title', formData.title);
-        fd.append('text', formData.text);
-        fd.append('category', formData.categories);
-        fd.append('views', 0);
-        if(formData.attachments)
-            for(var i=0; i<formData.attachments.length; i++){
-                fd.append('attachments', formData.attachments[i], formData.attachments[i].name);
-            }
-        
-        //Without this, jQuery sends the categories as "category[]"
-        $.ajaxSettings.traditional = true;
-        $.ajax({
-            url: "/ideas",
-            method: "POST",
-            contentType: false,
-            processData: false,
-            data: fd,
-            success: function(result){
-                browserHistory.push('/new/confirm');
-            },
-            error: function(xhr, status, err){
-                //this should never happen because the user can't see this page unless they are logged in
-                if(xhr.status == 302){
-                    browserHistory.push('/login')
+        var agreement = confirm("By submitting this idea you agree that you may be contacted by a moderator");
+        if(agreement){
+            var fd = new FormData();
+            fd.append('title', formData.title);
+            fd.append('text', formData.text);
+            fd.append('category', formData.categories);
+            fd.append('views', 0);
+            if(formData.attachments)
+                for(var i=0; i<formData.attachments.length; i++){
+                    fd.append('attachments', formData.attachments[i], formData.attachments[i].name);
                 }
-            }
-        }) 
+            
+            //Without this, jQuery sends the categories as "category[]"
+            $.ajaxSettings.traditional = true;
+            $.ajax({
+                url: "/ideas",
+                method: "POST",
+                contentType: false,
+                processData: false,
+                data: fd,
+                success: function(result){
+                    browserHistory.push('/new/confirm');
+                },
+                error: function(xhr, status, err){
+                    //this should never happen because the user can't see this page unless they are logged in
+                    if(xhr.status == 302){
+                        browserHistory.push('/login')
+                    }
+                }
+            })
+        }
     }
 
     componentDidMount() {
