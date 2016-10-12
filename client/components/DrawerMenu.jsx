e1b942d6ad64ac9c927bed797a3de97f3d15f007
Anne Faber <anne.faber@txstate.edu>
2016-10-12 16:26:54 -0500

    First draft of mobile drawer menu

---
+++ client/components/DrawerMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class DrawerMenu extends React.Component {
+
+    render(){
+        var currentPage = encodeURIComponent(this.props.currentPage);
+        return(
+            <nav role="navigation" className={"mobile-drawer " + (this.props.visible? "visible" : "") }>
+                <h3 className="mobile-drawer-title">Voting Application</h3>
+                <ul>
+                    {!this.props.auth.loggedIn && <li><a className="drawer-menu-item" href={"/login?url=" + currentPage}>Login</a></li>}
+                    <li><a className="drawer-menu-item" href="/admin">Admin</a></li>
+                    {this.props.auth.loggedIn && <li><a className="drawer-menu-item" href={"/logout"}>Log Out</a></li>}
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default DrawerMenu;
--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Login from './Login.jsx'; 
+import DrawerMenu from './DrawerMenu.jsx'; 
+import $ from 'jquery';
 
 class Layout extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuVisible: false
+        };
+    }
     
+    showMenu(){
+        this.setState({'menuVisible': true})
+    }
+
+    hideMenu(){
+        this.setState({'menuVisible': false})
+    }
+
+    handleClick(e){
+        var target = $(e.target);
+        if (this.state.menuVisible && !target.is('.hamburger') && !target.closest('.mobile-drawer').length) {
+          e.preventDefault();
+          this.hideMenu();
+        }
+    }
+
     render(){
         //pass auth to all children
         var _this = this;
@@ -13,17 +38,22 @@ class Layout extends React.Component {
             })
         });
         return(
-            <div className="app">
+            <div className="app" onClick={this.handleClick.bind(this)}>
                 <header>
                 <Login
                     auth = {this.props.auth}
                     currentPage = {this.props.location.pathname}
                 />
                 </header>
+                <DrawerMenu
+                    auth = {this.props.auth}
+                    visible = {this.state.menuVisible}
+                    currentPage = {this.props.location.pathname}
+                />
                 <div className="title-bar">
                     <i className="logo fa fa-check-square-o"></i>
                     <Link to="/" className="app-title">Voting Application</Link>
-                    <i className="fa fa-bars hamburger pull-right"></i>
+                    <a onClick={this.showMenu.bind(this)}><i className="fa fa-bars hamburger pull-right"></i></a>
                 </div>
                 {children}
             </div>
